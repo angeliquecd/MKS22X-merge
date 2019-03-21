4e398b88572ad9c89f9c6a22c0417a7e5b24221a
angeliquecd <acharles-davis00@stuy.edu>
2019-03-21 06:33:11 -0400

    something sorted

--- Merge.java
+++ Merge.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 public class Merge{
   public static void mergesort(int[] data){
+    mergehelper(data,data);}
+    public static void mergehelper(int[] data, int[] stored){
     int hi =data.length;
     int lo = 0;
     if (hi<=1) return ;
@@ -13,18 +15,18 @@ public class Merge{
     for (int h=0;h<data.length;h++){
     //  System.out.println(h);
       if (h<leftlength) leftside[h]=data[h];
-      else rightside[h-leftlength]=data[h];//fills up correct arrays
+      else rightside[h-leftlength]=data[h];//fills up correct arrays and splits in half
     }
-    mergesort(rightside);
-    mergesort(leftside);//changed wrapper function because i was having trouble with return values
-    data=merge(leftside,rightside);
+    mergehelper(rightside,stored);
+    mergehelper(leftside,stored);//changed wrapper function because i was having trouble with return values
+    data=merge(leftside,rightside, data);
   }
-    public static int[] merge (int[] rightside, int[] leftside){
+    public static int[] merge (int[] rightside, int[] leftside, int[] empty){
     //merge part
-    int total = rightside.length + leftside.length;
-    int[] empty=new int[total];
-    int i =0;
-    int j=0;
+    int total = rightside.length + leftside.length;//total length
+  //  int[] empty=new int[total];
+    int i =0;//for left
+    int j=0;//for right
     int inc =0;
     //    System.out.println("left side: ");
       //  printarray(leftside);
@@ -32,19 +34,19 @@ public class Merge{
       //  printarray(rightside);
     while (inc<total){
     if(i<leftside.length && j<rightside.length){
-      if (leftside[i]>rightside[j]){//compares stuff
-      empty[inc]=rightside[j];
-      j++;}//increments within the right list
+      if (leftside[i]<rightside[j]){//compares stuff
+      empty[inc]=leftside[i];
+      i++;}//increments within the right list
       else{
-        empty[inc]=leftside[i];
-        i++;}
+        empty[inc]=rightside[j];
+        j++;}
       inc++;//moves forward in new array
     //System.out.println("final: ");
   //  printarray(data);
-//    System.out.println("left side: ");
-  //  printarray(leftside);
-//    System.out.println("right side: ");
-  //  printarray(rightside);
+    System.out.println("left side: ");
+  printarray(leftside);
+    System.out.println("right side: ");
+    printarray(rightside);
 }
 else {
   if (i>=leftside.length){
@@ -54,13 +56,14 @@ else {
   }
   if (j>=rightside.length){
     while(i<leftside.length){
-      empty[inc]=leftside[i];//same as above but for new array
+      empty[inc]=leftside[i];//same as above but for other side
       i++;
     }
   }
   inc++;
 }
 }
+    System.out.println("total array: ");
 printarray(empty);
 return empty;
 }
